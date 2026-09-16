63e7a0115251a32e5e5b27addac2d2c35afe8a6d
ThiagoAlbqq <thiago.ce.al@gmail.com>
2026-09-15 22:40:33 -0300

    fix: compatibility target for older browsers

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,5 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true
+  },
+  esbuild: {
+    target: 'es2015'
+  },
+  build: {
+    target: 'es2015'
+  }
 })
 
